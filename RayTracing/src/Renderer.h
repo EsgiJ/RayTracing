207befe121832ac82fdff8e7b79ae8fb89bc5254
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2024-09-03 19:50:57 +0300

    Tutorial 6

---
+++ RayTracing/src/Renderer.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <memory>
+#include <glm/vec2.hpp>
+#include <glm/vec4.hpp>
+
+#include "Ray.h"
+#include "Camera.h"
+#include "Walnut/Image.h"
+
+class Renderer
+{
+public:
+	Renderer() = default;
+
+	void OnResize(uint32_t width, uint32_t height);
+	void Render(const Camera& camera);
+
+	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+private:
+	glm::vec4 TraceRay(const Ray& ray);
+private:
+	std::shared_ptr<Walnut::Image> m_FinalImage;
+	uint32_t* m_ImageData = nullptr;
+};
